c6472320d35717de27d31b975783a9084668e586
Amacrel <alexandre.macrel@gmail.com>
2023-09-08 11:42:45 +0200

    [Rebase] Rebase from Main

--- src/main/java/amacrel/it_training/entity/Category.java
+++ src/main/java/amacrel/it_training/entity/Category.java
@@ -1,4 +1,30 @@
 package amacrel.it_training.entity;
 
-public class Category {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int category_id;
+    @Column(name = "name")
+    private String category_name;
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(name = "Topic",
+            joinColumns = { @JoinColumn(name = "Id_Topic")},
+            inverseJoinColumns = { @JoinColumn(name = "Id_Category")}
+    )
+    private List<Topic> topics = new ArrayList<Topic>();
+
 }
