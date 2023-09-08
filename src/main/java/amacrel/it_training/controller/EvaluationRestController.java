a9da2bd50c70014603373aebdc0ddadaf00469ee
QuentinHumbert <quentinhum76@gmail.com>
2023-09-08 11:13:40 +0200

    [API] Create all Rest Controllers

---
+++ src/main/java/amacrel/it_training/controller/EvaluationRestController.java
@@ -0,0 +1,9 @@
+package amacrel.it_training.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/evaluations")
+public class EvaluationRestController {
+}
