c8e7b325ce0b72a2000adb9022f9ae6f1f5996fa
QuentinHumbert <quentinhum76@gmail.com>
2023-09-13 16:16:57 +0200

    [Controller] CRUD on controllers (Topic excluded) + SubtopicDao fix

--- src/main/java/amacrel/it_training/controller/RoleRestController.java
+++ src/main/java/amacrel/it_training/controller/RoleRestController.java
@@ -1,9 +1,48 @@
 package amacrel.it_training.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import amacrel.it_training.dao.RoleDao;
+import amacrel.it_training.entity.Role;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/roles")
+@Data
 public class RoleRestController {
+
+    private RoleDao roleDao;
+
+    public RoleRestController(RoleDao roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    @GetMapping("/all")
+    public List<Role> getRoles() {
+        return this.roleDao.getRoles();
+    }
+
+    @GetMapping("/{id}/")
+    public Optional<Role> getRoleById(@PathVariable int id) {
+        return this.roleDao.getRoleById(id);
+    }
+
+    @PostMapping("/create")
+    public void createRole(@RequestBody Role role) {
+        this.roleDao.createRole(role);
+    }
+
+    @PutMapping("/update")
+    public void updateRole(@RequestBody Role role) {
+        this.roleDao.updateRole(role);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteRole(@RequestBody Role role) {
+        this.roleDao.deleteRole(role);
+    }
+
+
 }
--- src/main/java/amacrel/it_training/controller/_UserRestController.java
+++ src/main/java/amacrel/it_training/controller/_UserRestController.java
@@ -1,9 +1,45 @@
 package amacrel.it_training.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import amacrel.it_training.dao._UserDao;
+import amacrel.it_training.entity._User;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Data
 public class _UserRestController {
+    private _UserDao userDao;
+
+    public _UserRestController(_UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/all")
+    public List<_User> getUsers() {
+        return this.userDao.getUsers();
+    }
+
+    @GetMapping("/{id}/")
+    public Optional<_User> getUserById(@PathVariable int id) {
+        return this.userDao.getUserById(id);
+    }
+
+    @PostMapping("/create")
+    public void createUser(@RequestBody _User user) {
+        this.userDao.createUser(user);
+    }
+
+    @PutMapping("/update")
+    public void updateUser(@RequestBody _User user) {
+        this.userDao.updateUser(user);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteUser(@RequestBody _User user) {
+        this.userDao.deleteUser(user);
+    }
 }
